89d659da8f74f7f9651bf35c269ea8f412377b5b
Dominic Cheung <dom.with.a.inic@gmail.com>
2015-10-15 01:51:26 +1100

    Add gradient triangle behind logo image

--- Hypnosister-Swift/HypnosisView.swift
+++ Hypnosister-Swift/HypnosisView.swift
@@ -37,19 +37,41 @@ class HypnosisView: UIView {
         }
         path.stroke()
         
-        let imageRect = CGRectMake(bounds.origin.x + bounds.width/4, bounds.origin.y + bounds.height/4, bounds.width/2, bounds.height/2)
+        let imageRect = CGRectMake(bounds.origin.x + bounds.size.width/4, bounds.origin.y + bounds.size.height/4, bounds.size.width/2, bounds.size.height/2)
+        
+        let currentContent = UIGraphicsGetCurrentContext()
+        
+        // Draw Gradient
+        CGContextSaveGState(currentContent)
+        
+        // Draw triangle path for gradient
+        let trianglePath = UIBezierPath()
+        trianglePath.moveToPoint(CGPoint(x: bounds.origin.x + bounds.size.width/2.0, y: bounds.origin.x + bounds.size.height/4.0))
+        trianglePath.addLineToPoint(CGPoint(x: bounds.origin.x + bounds.size.width/4.0, y: bounds.origin.x + bounds.size.height/4.0*3.0))
+        trianglePath.addLineToPoint(CGPoint(x: bounds.origin.x + bounds.size.width/4.0*3.0, y: bounds.origin.x + bounds.size.height/4.0*3.0))
+        trianglePath.closePath()
+        trianglePath.addClip()
+        
+        let locations: [CGFloat] = [0.0, 1.0]
+        let components: [CGFloat] = [
+            1.0, 0.0, 0.0, 1.0,
+            1.0, 1.0, 0.0, 1.0]
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let gradient = CGGradientCreateWithColorComponents(colorSpace, components, locations, 2)
+        let startPoint = CGPoint(x: bounds.origin.x + bounds.width/4, y: bounds.origin.y + bounds.height/4)
+        let endPoint = CGPoint(x: bounds.origin.x + bounds.width/4*3, y: bounds.origin.y + bounds.height/4*3)
+        
+        CGContextDrawLinearGradient(currentContent, gradient, startPoint, endPoint, CGGradientDrawingOptions(rawValue: 0))
+        
+        CGContextRestoreGState(currentContent)
         
         // Draw Drop Shadow
-        var currentContent = UIGraphicsGetCurrentContext()
-        
         CGContextSaveGState(currentContent)
         CGContextSetShadow(currentContent, CGSizeMake(4, 7), 3)
         
         self.logoImage?.drawInRect(imageRect)
         
         CGContextRestoreGState(currentContent)
-        
-        
     }
 
 }
