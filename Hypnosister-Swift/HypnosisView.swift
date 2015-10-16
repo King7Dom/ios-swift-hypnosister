5691ebb1257be076c69493762de6ec5031a86b85
Dominic Cheung <dom.with.a.inic@gmail.com>
2015-10-16 22:31:43 +1100

    Change circleCircle on touch

--- Hypnosister-Swift/HypnosisView.swift
+++ Hypnosister-Swift/HypnosisView.swift
@@ -11,7 +11,11 @@ import UIKit
 
 class HypnosisView: UIView {
     
-    var circleColor = UIColor.lightGrayColor()
+    var circleColor: UIColor = UIColor.lightGrayColor() {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
     
     override required init(frame: CGRect) {
         super.init(frame: frame)
@@ -37,6 +41,15 @@ class HypnosisView: UIView {
             path.addArcWithCenter(center, radius: radius, startAngle: 0.0, endAngle: CGFloat(M_PI) * 2.0, clockwise: true)
         }
         path.stroke()
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let red = CGFloat(arc4random() % 100) / 100.0
+        let green = CGFloat(arc4random() % 100) / 100.0
+        let blue = CGFloat(arc4random() % 100) / 100.0
+        let randomColor = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+        
+        self.circleColor = randomColor
     }
 
 }
