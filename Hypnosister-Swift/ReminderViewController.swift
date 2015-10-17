719232bc977282502eafe42ca076c9f0f65d8848
Dominic Cheung <dom.with.a.inic@gmail.com>
2015-10-17 19:41:26 +1100

    Add tabBarItem to ReminderViewController

--- Hypnosister-Swift/ReminderViewController.swift
+++ Hypnosister-Swift/ReminderViewController.swift
@@ -13,6 +13,17 @@ class ReminderViewController: UIViewController {
     @IBOutlet private weak var datePicker: UIDatePicker?
     @IBOutlet private weak var remindButton: UIButton?
 
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+
+        self.tabBarItem.title = "Reminder"
+        self.tabBarItem.image = UIImage(named: "time")
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
